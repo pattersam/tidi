552e20f2e2217be77fab213f9b3ca4799c569ac9
Sam Patterson <samuel.patterson@prosiebensat1.com>
2023-10-10 15:49:31 +0200

    fix: make registry thread safe
    as in, don't share the global state across threads
    
    this implicitly results in the objects being _scoped_ to a thread
    
    in the future, we can consider elevating the newly added `_Container`
    concept to reflect other _scopes_ as well like Singleton or NoScope

--- src/tidi/registry.py
+++ src/tidi/registry.py
@@ -1,6 +1,7 @@
 """Provides a `TidiRegistry`, responsible for providing stored dependencies."""
 
 import builtins
+import threading
 import typing as t
 
 T = t.TypeVar("T")
@@ -21,16 +22,39 @@ class RegistryLookupError(LookupError):
     """Error finding desired type in registry"""
 
 
+class _Container(t.Protocol):
+    def add(self, obj: T):
+        ...  # pragma: no cover
+
+    def get(self, type_: t.Type[T], default: T | None = None) -> T:
+        ...  # pragma: no cover
+
+
+class _PerThreadContainer(threading.local):
+    def __init__(self):
+        self._map = {}
+
+    def add(self, obj: T):
+        self._map[type(obj)] = obj
+
+    def get(self, type_: t.Type[T], default: T | None = None) -> T:
+        return self._map.get(type_, default)
+
+
 class TidiRegistry:
     """A simple registry of objects indexed by their type."""
 
-    def __init__(self, banned_types: list[t.Type] | None = None):
+    def __init__(
+        self,
+        banned_types: list[t.Type] | None = None,
+        container_cls: t.Type[_Container] = _PerThreadContainer,
+    ):
         if banned_types is None:
             banned_types = [
                 type_ for type_ in builtins.__dict__.values() if isinstance(type_, type)
             ]
         self.banned_types = banned_types
-        self._registry: dict = {}
+        self._container = container_cls()
 
     def register(self, obj: t.Any):
         """Register an instance `obj` of class `T` to be available for injection.
@@ -44,7 +68,7 @@ class TidiRegistry:
         type_ = type(obj)
         if type_ in self.banned_types:
             raise RegistrationError(f"Trying to register a banned type: {type_}")
-        self._registry[type_] = obj
+        self._container.add(obj)
 
     def get(self, type_: t.Type[T], default: t.Any = _unknown) -> T:
         """Get an instance of type `type_` from the regsitry.
@@ -60,7 +84,7 @@ class TidiRegistry:
         Returns:
             (type_ (T)): the registered object, or default value if it was provided.
         """
-        obj = self._registry.get(type_, default)
+        obj = self._container.get(type_, default)
         if isinstance(obj, _Unknown):
             raise RegistryLookupError(f"Type has not been registered: {type_}")
         return obj
